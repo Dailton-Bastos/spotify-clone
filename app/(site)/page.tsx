b3cbcf4f07c73cb33c1e9262b0fd2a41222846a8
Dailton Bastos <dailtonbastos@gmail.com>
2023-06-09 23:15:45 -0300

    feat: add error and loading component

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,9 +1,9 @@
+export const revalidate = 0;
+
 import { getSongs } from '@/actions/getSongs';
 import { Header } from '@/components/Header';
 import { ListItem } from '@/components/ListItem';
 import { PageContent } from './components/PageContent';
-
-const revalidate = 0;
 
 export default async function Home() {
   const songs = await getSongs();
